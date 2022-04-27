ee3532cfb64f8e128d5a4e109f9c476b496276a8
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2022-04-27 21:39:15 +0200

    Setup unit testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.utils.extendsFrom
+
 plugins {
     kotlin("jvm") version "1.6.21"
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -20,6 +22,18 @@ dependencies {
 
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
     implementation(project(":api"))
+
+    testImplementation("io.mockk:mockk:1.12.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.testcontainers:testcontainers:1.17.1")
+    testImplementation("org.testcontainers:junit-jupiter:1.17.1")
+}
+
+configurations {
+    arrayOf(compileOnly, implementation).forEach {
+        testImplementation.extendsFrom(it)
+    }
 }
 
 tasks {
@@ -33,6 +47,9 @@ tasks {
         kotlinOptions {
             jvmTarget = "17"
         }
+    }
+    test {
+        useJUnitPlatform()
     }
 }
 
---
+++ src/test/kotlin/io/phoenix/sulfur/plugin/JavaSulfurTest.kt
@@ -0,0 +1,106 @@
+package io.phoenix.sulfur.plugin
+
+import io.mockk.*
+import io.phoenix.sulfur.api.SulfurPlugin
+import org.bukkit.Bukkit
+import org.bukkit.plugin.PluginManager
+import org.junit.jupiter.api.*
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import java.util.*
+
+@Testcontainers
+class JavaSulfurTest {
+    @Container
+    val redis = GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+        .withExposedPorts(6379)
+
+    lateinit var database: SulfurDatabase
+    lateinit var plugin: SulfurPlugin
+
+    @BeforeEach
+    fun setUp() {
+        database = SulfurDatabase(redis.host, redis.firstMappedPort)
+
+        plugin = mockk()
+        every { plugin.onRegisterGame(any()) } returns Unit
+        every { plugin.name } returns "UHC Sample"
+
+        mockkStatic(Bukkit::class)
+        val pm: PluginManager = mockk()
+        every { pm.getPlugin("UHC Sample") } returns plugin
+        every { Bukkit.getPluginManager() } returns pm
+    }
+
+    @Test
+    fun basic() {
+        val host = UUID.randomUUID()
+
+        val game = database.registerGame(host, plugin, "azerty")
+
+        verify(exactly = 1) { plugin.onRegisterGame(any()) }
+        Assertions.assertEquals("azerty", game.server())
+        Assertions.assertEquals("UHC Sample", game.plugin().name)
+        Assertions.assertEquals(host, game.host().id)
+    }
+
+    @Test
+    fun metadata() {
+        val game = database.registerGame(UUID.randomUUID(), plugin)
+
+        game.metadata["foo"] = "bar"
+        game.metadata["baz"] = "qux"
+
+        Assertions.assertEquals("qux", game.metadata["baz"])
+        Assertions.assertEquals("bar", game.metadata["foo"])
+        Assertions.assertEquals(null, game.metadata["none"])
+
+        game.metadata.clear()
+
+        Assertions.assertEquals(null, game.metadata["baz"])
+
+        game.metadata["a"] = "1"
+        game.metadata["b"] = "2"
+        game.metadata["a"] = "3"
+
+        Assertions.assertEquals("3", game.metadata["a"])
+
+        game.metadata.remove("b")
+
+        Assertions.assertEquals("3", game.metadata["a"])
+        Assertions.assertEquals(null, game.metadata["b"])
+    }
+
+    @Test
+    fun players() {
+        val game = database.registerGame(UUID.randomUUID(), plugin)
+
+        val i1 = UUID.randomUUID()
+        val i2 = UUID.randomUUID()
+        val i3 = UUID.randomUUID()
+
+        val p1 = game.addPlayer(i1)
+        val p2 = game.addPlayer(i2)
+        val p3 = game.addPlayer(i3)
+
+        p1.metadata["foo"] = "bar"
+        p2.dead(true)
+        p3.delete()
+
+        Assertions.assertEquals(2, game.players().size)
+
+        Assertions.assertEquals("bar", p1.metadata["foo"])
+        Assertions.assertEquals(null, p1.metadata["none"])
+        Assertions.assertEquals(null, p2.metadata["foo"])
+        Assertions.assertEquals(null, game.metadata["foo"])
+
+        Assertions.assertEquals(false, p1.dead())
+        Assertions.assertEquals(true, p2.dead())
+
+        Assertions.assertEquals(true, p1.exists())
+        Assertions.assertEquals(true, p2.exists())
+        Assertions.assertEquals(false, p3.exists())
+    }
+}
